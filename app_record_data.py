de016d8ac709d5c350c6457b18551970e4a70e1f
Jamie-McKernan <jamie.mckernan@gmail.com>
2021-03-28 21:27:01 +0100

    replaced random with temp sensor

--- app_record_data.py
+++ app_record_data.py
@@ -5,11 +5,25 @@ import pause
 import datetime as dt
 import statistics as stat
 
-from random import randint
+
+# Sensors Import
+from bme280 import BME280
+
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+
+### Sensor Setup
+
+# temp
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+
 
 
 def get_temp():
-    return (randint(1800, 2500)/100)
+    return bme280.get_temperature()
 
 
 def get_data_sample(data_samples_for_average):
@@ -18,9 +32,8 @@ def get_data_sample(data_samples_for_average):
     for key in data_samples_for_average.keys():
         data_samples_for_average[key] = []
 
-    for _ in range(10):
+    for _ in range(30):
         data_samples_for_average["temp"].append(get_temp())
-        print("getting temp data")
         time.sleep(1)
 
     return_data = {}
@@ -63,11 +76,11 @@ def record_data():
         next_minute = now_adjusted + dt.timedelta(0, 60)
 
         time_string = f"{str(now_adjusted.hour).zfill(2)}{str(now_adjusted.minute).zfill(2)}"
-        print("getting data sample")
+        
         data_sample = get_data_sample(all_data_names)
         data_sample["piid"] = piid
         data_sample["time"] = time_string
-        print("got data sample, now writing...")
+
         with open(f"./data/{piid}.csv", 'a') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writerow(data_sample)
